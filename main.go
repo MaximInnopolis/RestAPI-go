bdee3f742624e1a0cf92b42337dde3e9e03dd898
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2022-12-22 23:50:36 +0300

    auth and adding users created

--- main.go
+++ main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
 )
 
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type User struct {
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Login     string `json:"login"`
+	Password  string `json:"password"`
+	BirthDate string `json:"birth_date"`
+}
+
+func mainAdmin(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, admin! You are on the admin main page")
+}
+
+func addUser(c echo.Context) error {
+	user := User{}
+
+	err := c.Bind(&user)
+	if err != nil {
+		log.Printf("Failed processing addUser request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("User added: %#v", user)
+	return c.String(http.StatusOK, "User added")
 }
 
 func main() {
 	// Echo instance
 	e := echo.New()
 
+	g := e.Group("/admin")
+
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	g.Use(middleware.Logger())
+	g.Use(middleware.Recover())
+
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "admin" && password == "admin" {
+			return true, nil
+		}
+
+		return false, nil
+	}))
 
 	// Routes
-	e.GET("/", hello)
+	g.GET("/main", mainAdmin)
+	g.POST("/users", addUser)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
